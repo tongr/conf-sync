9aeac2e011a0b9bed0d34cd6b68aa383fbc7cb8d
tongr <tongr@github.com>
2015-10-12 14:10:52 +0200

    added symbolic link functions

--- setup.sh
+++ setup.sh
@@ -1,32 +1,85 @@
 #! /bin/bash
-SCRIPT_DIR="$(cd "${BASH_SOURCE[0]%/*}"; pwd)"
+set -e
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SH_SCRIPT_DIR="$ROOT_DIR/shell_scripts"
+LN_DIR="$ROOT_DIR/links"
 COMMENT="# executing synchronized script"
+
+#
+# util functions
+#
+yes_no () {
+  MSG="$1"
+  RESULT=""
+  while [ "y" != "$RESULT" -a "n" != "$RESULT" ]; do
+    read -p "$MSG: " input
+    RESULT=${input:-$DEFAULT}
+  done
+  echo "$RESULT"
+}
+read_config () {
+  RESULT=()
+  while read line || [ -n "$line" ]; do
+    if [[ ! -z  $line ]] && [[ ! $line =~ ^[[:space:]]*#.*$ ]]; then
+      RESULT+=("$line")
+    fi
+  done < $1
+  echo "$RESULT"
+}
 
 echo "Setting up synchronized scripts from $SCRIPT_DIR ..."
 
-while read line || [ -n "$line" ]
-do
-  if [[ ! -z  $line ]] && [[ ! $line =~ ^[[:space:]]*#.*$ ]]
-  then
-    KV=(${line//=/ })
-    SOURCE="$SCRIPT_DIR/${KV[0]}"
-    DESTINATION="${KV[1]}"
-    echo "Installing $SOURCE scripts ..."
-    for SCRIPT in $(ls -a $SOURCE)
-    do
-      CALL_LINE="source $SOURCE/$SCRIPT"
-      if [ ! -d "${SCRIPT}" ]
-      then
-        if [ ! -e $DESTINATION/$SCRIPT ]
-        then
-          echo -e "$COMMENT\n$CALL_LINE" > $DESTINATION/$SCRIPT
-        elif ! grep -q "$CALL_LINE" "$DESTINATION/$SCRIPT"
-        then
-          echo -e "$COMMENT\n$CALL_LINE" >> $DESTINATION/$SCRIPT
+#
+# setup symbolic links
+#
+answer=$(yes_no "Do you want to install config links?")
+if [ "y" == "$answer" ]; then
+  cfgs=$(read_config "$LN_DIR/sync.conf")
+  for i in "${cfgs[@]}"; do
+    KV=(${i//=/ })
+    SOURCE="$LN_DIR/${KV[0]}"
+    DESTINATION="${KV[1]/#\~/$HOME}"
+    DESTINATION="$(mkdir -p $DESTINATION;cd $DESTINATION;pwd)"
+    for SCRIPT in $(ls -a "$SOURCE"); do
+      if [ ! -d "${SCRIPT}" ];then
+        if [ -e "$DESTINATION/$SCRIPT" ]; then
+          answer=$(yes_no "Do you want to overwrite the existing file $DESTINATION/$SCRIPT with $SOURCE/$SCRIPT?")
+          if [ "y" = "$answer" ]; then
+            rm "$DESTINATION/$SCRIPT"
+            ln -s "$SOURCE/$SCRIPT" "$DESTINATION/$SCRIPT"
+          fi
+        else
+          ln -s "$SOURCE/$SCRIPT" "$DESTINATION/$SCRIPT"
         fi
       fi
     done
-  fi
-done <$SCRIPT_DIR/sync.conf
+  done
+fi
+
+#
+# setup shell scripts
+#
+answer=$(yes_no "Do you want to install shell sources?")
+if [ "y" == "$answer" ]; then
+  cfgs=$(read_config "$SH_SCRIPT_DIR/sync.conf")
+  for i in "${cfgs[@]}"; do
+    KV=(${i//=/ })
+    SOURCE="$SH_SCRIPT_DIR/${KV[0]}"
+    DESTINATION="${KV[1]}"
+    for SCRIPT in $(ls -a "$SOURCE"); do
+      CALL_LINE="source $SOURCE/$SCRIPT"
+      if [ ! -d "${SCRIPT}" ]; then
+        if [ ! -e "$DESTINATION/$SCRIPT" ]; then
+          answer=$(yes_no "Destination $DESTINATION/$SCRIPT for source script $SOURCE/$SCRIPT does not exist. Do you want to create the destination?")
+          if [ "y" = "$answer" ]; then
+            echo -e "$COMMENT\n$CALL_LINE" > "$DESTINATION/$SCRIPT"
+          fi
+        elif ! grep -q "$CALL_LINE" "$DESTINATION/$SCRIPT"; then
+          echo -e "$COMMENT\n$CALL_LINE" >> "$DESTINATION/$SCRIPT"
+        fi
+      fi
+    done
+  done
+fi
 
 echo "Setup finished!"
